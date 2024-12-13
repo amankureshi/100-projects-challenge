a94c277eee557a7be78ab3b237f2b0182d42c111
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-13 21:30:56 +0530

    added plus minus opertaion & reset

--- day-17/src/App.jsx
+++ day-17/src/App.jsx
@@ -1,29 +1,55 @@
-import { IoIosRefresh } from "react-icons/io";
+import { IoMdRefresh } from "react-icons/io";
 import "./App.css";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import { useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleClick1 = () => {
+    setCounter(counter + 1);
+  };
+  const handleClick2 = () => {
+    if (counter === 0) {
+      return 0;
+    } else {
+      setCounter(counter - 1);
+    }
+  };
+  const reseteClick = () => {
+    setCounter(counter === 0);
+    window.location.reload();
+  };
+
   return (
     <>
       <div className="app">
         <div className="container">
+          <div className="right">
+            <IoMdRefresh onClick={reseteClick} />
+          </div>
           <div className="total_amount_card">
-            <div className="right">
-              <IoIosRefresh />
-            </div>
             <div className="card_text">
-              <div className="total_amount_heading">1</div>
+              <div className="total_amount_heading">{counter}</div>
             </div>
           </div>
           <form>
             <div className="btn_collection">
               <Stack spacing={2} direction="row">
-                <Button variant="contained" className="btn_one">
-                  +
+                <Button
+                  onClick={handleClick1}
+                  variant="contained"
+                  className="btn_one"
+                >
+                  <FaPlus />
                 </Button>
-                <Button variant="contained" className="btn_two">
-                  -
+                <Button
+                  onClick={handleClick2}
+                  variant="contained"
+                  className="btn_two"
+                >
+                  <FaMinus />
                 </Button>
               </Stack>
             </div>
