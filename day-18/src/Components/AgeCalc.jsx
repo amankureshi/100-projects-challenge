d290c10ecca2fb45faa949f936c5ea1998250bf1
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-16 12:11:12 +0530

    logic completed

--- day-18/src/Components/AgeCalc.jsx
+++ day-18/src/Components/AgeCalc.jsx
@@ -1,26 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AgeCalc.css";
 const AgeCalc = () => {
+  const [birthDate, setBirthDate] = useState("");
+  const [age, setAge] = useState(0);
+  const calculateAge = () => {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDiff = today.getMonth() - birthDateObj.getMonth();
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDateObj.getDate())
+    ) {
+      age--;
+    }
+    setAge(age);
+    console.log(age);
+  };
+  const calculateReset = () => {
+    setBirthDate("");
+    setAge();
+  };
+
   return (
     <>
-      <h1>Age Calculator</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <div className="container">
-        <div className="right">
-          <h2>Date of Birth</h2>
-          <div className="btn_div">
-            <input className="date" type="date" />
-            <button className="btn_age">Calculate Age</button>
-            <button className="btn_age">Reset</button>
+      <section className="container">
+        <h1>Age Calculator</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <div className="container_middle">
+          <div className="right">
+            <div className="btn_div">
+              <input
+                className="date"
+                type="date"
+                value={birthDate}
+                onChange={(e) => {
+                  setBirthDate(e.target.value);
+                }}
+              />
+              <button className="btn_age" onClick={calculateAge}>
+                Calculate Age
+              </button>
+              <button className="btn_age" onClick={calculateReset}>
+                Reset
+              </button>
+            </div>
+            <h2>Date of Birth</h2>
+          </div>
+          <div className="left">
+            <div className="container_fluid">
+              <h1>Age</h1>
+            </div>
+            <div className="age_heading">{age > 0 ? `${age}` : ""}</div>
           </div>
         </div>
-        <div className="left">
-          <div className="container_fluid">
-            <h1>Age</h1>
-          </div>
-          <div className="age_heading">20</div>
-        </div>
-      </div>
+      </section>
     </>
   );
 };
