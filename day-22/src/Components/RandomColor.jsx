f575758df14c7405b816f9f99635bd89580cc3e9
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-25 18:38:34 +0530

    implement aHex color logic

--- day-22/src/Components/RandomColor.jsx
+++ day-22/src/Components/RandomColor.jsx
@@ -24,17 +24,20 @@ const RandomColor = () => {
 
         setColor(`rgb(${r},${g},${b})`);
     }
+    function handleHexaColorbtn() {
+        handleCreateRandomHexColor();
+    }
 
     return (
         <div className='random_body' style={{ width: "100vw", height: "100vh", background: color, }}>
-            <button onClick={() => setTypeofColor('hex')}>Create HEX Color</button>
+            <button onClick={handleHexaColorbtn}>Create HEX Color</button>
             <button onClick={() => setTypeofColor('rgb')}>Create RGB Color</button>
             <button onClick={typeofColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor}>Generator Random Color </button>
             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", fontSize: '2rem', marginTop: "4rem", flexDirection: "column", gap: "20px" }}>
                 <h3>{typeofColor === 'rgb' ? "RGB Color" : "HEX color"}</h3>
                 <h1>{color}</h1>
             </div>
-        </div>
+        </div >
     )
 }
 
