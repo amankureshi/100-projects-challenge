6ee7fa7c25dea25d6d26f3626d85657242c1b35b
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-28 15:35:28 +0530

    remove hooks in app.jsx

--- day-23/src/app.jsx
+++ day-23/src/app.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'preact/hooks'
 import './app.css'
 import Star from './Components/Star'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
