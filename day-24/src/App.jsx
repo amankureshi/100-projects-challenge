413fce60385194a6d602d6c178493bfd1bfa1286
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-29 22:57:17 +0530

    crate componets and added joke file

---
+++ day-24/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Joke from './Components/Joke'
+
+function App() {
+
+  return (
+    <>
+      <h2>Joke generator</h2>
+      <Joke />
+    </>
+  )
+}
+
+export default App
---
+++ day-24/src/Components/Joke.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Joke = () => {
+    return (
+        <div>
+            <button>Click here</button>
+            <p></p>
+        </div>
+    )
+}
+
+export default Joke
