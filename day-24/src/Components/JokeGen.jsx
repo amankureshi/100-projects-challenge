374add0ed76f627b9acb2c4e2e8c8292e844fffa
Aman Kureshi <amankureshi78692@gmail.com>
2024-12-31 11:21:23 +0530

    implement jokeGen logic and appy a css

--- day-24/src/Components/JokeGen.jsx
+++ day-24/src/Components/JokeGen.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import Button from './Button';
 
 const JokeGen = () => {
     const [Joke, setJoke] = React.useState("");
     const fetchApi = () => {
         fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
             .then((res) => res.json())
-            .then((data) => setJoke(data.Joke));
+            .then((data) => setJoke(data.joke));
     };
     return (
         <div>
             <div className="joke">
+                <p>{Joke}</p>
+                <Button callApi={fetchApi} />
             </div>
         </div>
     )
