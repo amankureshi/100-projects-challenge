3dbaf99bc1df78f2fd2d689c76ba3d906a48fc40
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-02 00:55:34 +0530

    create a componet and implement logic in  MovieCard

--- day-25/src/App.jsx
+++ day-25/src/App.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from 'react';
 import { IoMdSearch } from "react-icons/io";
 import './App.css'
+import MovieCard from './assets/Components/MovieCard';
 
 const API_URL = "https://omdbapi.com?apikey=fe2f6c44";
 
-function App() {
+const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
-  const searMovies = async (title) => {
+  const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search)
@@ -16,20 +17,31 @@ function App() {
 
 
   useEffect(() => {
-    searMovies("SpiderMan");
+    searchMovies("SpiderMan");
   }, []);
 
   return (
-    <>
-      <div className="app">
-        <h1>Movie App</h1>
-        <div className="search">
-          <input type="text" placeholder='Search for movies' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-          <IoMdSearch onClick={() => searMovies(searchTerm)} />
-        </div>
+
+    <div className="app">
+      <h1>Movie App</h1>
+      <div className="search">
+        <input type="text" placeholder='Search for movies' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <IoMdSearch onClick={() => searchMovies(searchTerm)} />
       </div>
-    </>
-  )
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies Found</h2>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App
--- day-25/src/assets/Components/MovieCard.jsx
+++ day-25/src/assets/Components/MovieCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
     return (
-        <div>
-
+        <div className='movie'>
+            <div>
+                <p>{movie.year}</p>
+            </div>
+            <div className="image-section">
+                <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"} alt="movie-image" />
+            </div>
+            <div className="movie-contnet">
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
         </div>
     )
 }
