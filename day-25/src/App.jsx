c22c1157e591ebc3fda465f95d92527bf5b4a1c8
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-01 19:10:37 +0530

    added API and hooks

--- day-25/src/App.jsx
+++ day-25/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
+import { IoMdSearch } from "react-icons/io";
 import './App.css'
 
+const API_URL = "https://omdbapi.com?apikey=fe2f6c44";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search)
+  };
+
+
+  useEffect(() => {
+    searMovies("SpiderMan");
+  }, []);
 
   return (
     <>
-      <h1>Movie App</h1>
+      <div className="app">
+        <h1>Movie App</h1>
+        <div className="search">
+          <input type="text" placeholder='Search for movies' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+          <IoMdSearch onClick={() => searMovies(searchTerm)} />
+        </div>
+      </div>
     </>
   )
 }
