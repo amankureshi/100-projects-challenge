ddfe1119d9dcd57f7060addcb7b4459f5ce6ebe3
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-02 23:32:48 +0530

    implement a css and crate useEffect hooks and added favicon

--- day-25/src/App.jsx
+++ day-25/src/App.jsx
@@ -2,31 +2,23 @@ import { useEffect, useState } from 'react';
 import { IoMdSearch } from "react-icons/io";
 import './App.css'
 import MovieCard from './assets/Components/MovieCard';
+import useFetch from './hooks/useFetch';
 
 const API_URL = "https://omdbapi.com?apikey=fe2f6c44";
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("superman")
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    console.log(response);
+  const { data: movies, loading, error } = useFetch(`${API_URL}&s=${searchTerm}`);
+  const { data: qoute, loading: qouteLoading, error: qouteError } = useFetch(`https://zenquotes.io/api/today`);
 
-    const data = await response.json();
-    setMovies(data.Search)
-  };
-
-
-  useEffect(() => {
-    searchMovies("cartoon");
-    // console.log(setSearchTerm);
-  }, []);
+  console.log(qoute);
 
   return (
 
     <div className="app">
       <h1>Movie App</h1>
+      {/* {qouteLoading ? 'qoute loading' : qoute[0].q} */}
       <div className="search">
         <div className="search-container">
           <input
@@ -34,22 +26,36 @@ const App = () => {
             placeholder="Search for movies"
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
+            onKeyDown={(e) => {
+
+            }}
           />
           <IoMdSearch onClick={() => searchMovies(searchTerm)} />
         </div>
       </div>
 
-      {movies?.length > 0 ? (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard key={movie.imdbID} movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies Found</h2>
-        </div>
-      )}
+      <div className="container">
+        {loading ?
+          Array(10).fill(0)
+            .map((_, index) => (
+              <div className="movie" key={index}>
+                <div className='shimmer' style={{ height: '300px' }} />
+                <div className="movie-content">
+                  <h3 className='shimmer'>&nbsp;</h3>
+                  <span className='shimmer'>&nbsp;</span>
+                </div>
+              </div>
+            ))
+
+          : <>
+            {movies && (
+              movies?.Search?.map((movie) => (
+                <MovieCard key={movie.imdbID} movie={movie} />
+              ))
+            )}
+          </>
+        }
+      </div>
     </div>
   );
 }
