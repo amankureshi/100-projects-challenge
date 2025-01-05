4c2918148d0560f6a60a1a1a519e285dd87c322e
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-05 23:13:38 +0530

    api is working

--- day-27/src/Components/ImageSlider.jsx
+++ day-27/src/Components/ImageSlider.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-
+import { FaArrowCircleLeft, FaArrowCircleRight } from "react-icons/fa";
+import "../App.css"
 const ImageSlider = ({ url, limit = 5, page = 1 }) => {
     const [images, setImages] = useState([]);
     const [currentSlide, setCurrentslide] = useState(0);
@@ -8,22 +9,21 @@ const ImageSlider = ({ url, limit = 5, page = 1 }) => {
 
     async function fetchImages(getUrl) {
         try {
-            setLoading(true)
+            setLoading(false)
             const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
             const data = await response.json()
 
             console.log(data);
 
-
             if (Array.isArray(data)) {
                 setImages(data)
-            } else if (data.result) {
-                setImages(data.result)
+            } else if (data.results || data.result) {
+                setImages(data.results || data.result)
             }
             else {
                 setErrorMsg("API")
             }
-            setLoading(fa)
+            setLoading(false)
         } catch (e) {
             setErrorMsg(e.message)
             setLoading(false)
@@ -31,7 +31,7 @@ const ImageSlider = ({ url, limit = 5, page = 1 }) => {
     }
 
     useEffect(() => {
-        if (url !== '') fetchImages(url)
+        if (url !== '') fetchImages(url);
     }, [url])
 
     if (loading) {
@@ -48,11 +48,21 @@ const ImageSlider = ({ url, limit = 5, page = 1 }) => {
 
     return (
         <div className='container'>
-            {
-                images.map(imageItem =>
-                    <img src={imageItem.download_url} alt={imageItem} className='current-image' key={imageItem.id} />
-                )
+            <FaArrowCircleLeft className="arrow arrow-left" />
+            {images && images.length
+                ? images.map((imageItem) => (
+                    <img src={imageItem.download_url} alt={imageItem.download_url} className='current-image' key={imageItem.id || index} />
+                ))
+                : null
             }
+            <FaArrowCircleRight className="arrow arrow-left" />
+
+            <span className='circle-indicators'>
+                {images && images.length ?
+                    images.map((_, index) => (<button key={index} className='current-indicator'></button>))
+                    : null
+                }
+            </span>
         </div>
     )
 }
