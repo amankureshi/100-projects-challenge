4843d5b1fdaedcfbe62c700983aa11ed337ba589
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-11 23:12:56 +0530

    Add function to calculate and track scroll percentage

--- day-29/src/components/ScrollIndicators.jsx
+++ day-29/src/components/ScrollIndicators.jsx
@@ -1,54 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const ScrollIndicators = ({ url }) => {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("")
-    const [scrollPercentage, setScrollPercentage] = useState(0);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
-    async function fetchData(getUrl) {
-        try {
-            setLoading(true);
-            const response = await fetch(getUrl)
-            const data = await response.json();
+  async function fetchData(getUrl) {
+    try {
+      setLoading(true);
+      const response = await fetch(getUrl);
+      const data = await response.json();
 
-            if (data && data.products && data.products.length > 0) {
-                setData(data.products);
-                setLoading(false);
-            }
+      if (data && data.products && data.products.length > 0) {
+        setData(data.products);
+        setLoading(false);
+      }
 
-            // alert(data)
-            console.log(data);
-        } catch (e) {
-            console.log(e);
-            setErrorMessage(e.message)
-        }
+      // alert(data)
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+      setErrorMessage(e.message);
     }
+  }
 
-    useEffect(() => {
-        fetchData(url)
-    }, [url])
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
 
-    function handleScrollPercentage() {
-        console.log(document.body.scrollTop, document.documentElement.scrollTop, document.documentElement.scrollHeight, document.documentElement.clientHeight);
-    }
+  function handleScrollPercentage() {
+    console.log(
+      document.body.scrollTop,
+      document.documentElement.scrollTop,
+      document.documentElement.scrollHeight,
+      document.documentElement.clientHeight
+    );
 
-    useEffect(() => {
-        window.addEventListener('scroll', handleScrollPercentage)
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
 
-        return () => {
-            window.removeEventListener('scroll', () => { })
-        }
-    })
+    const height =
+      document.documentElement.scrollHeight -
+      documentElement.documentElement.clientHeight;
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  }
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
 
-    return (
-        <div className='container'>
-            <h1>Custom Scroll Indicator</h1>
-            <div className="data-content">
-                {data && data.length > 0 ? data.map((dataItem) => <p>{dataItem.title}</p>) : null}
-            </div>
-        </div>
-    )
-}
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  });
 
-export default ScrollIndicators;    
+  return (
+    <div className="container">
+      <h1>Custom Scroll Indicator</h1>
+      <div className="data-content">
+        {data && data.length > 0
+          ? data.map((dataItem) => <p>{dataItem.title}</p>)
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollIndicators;
