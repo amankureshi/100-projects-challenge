ef81f47fb279e507579c5264d297f34ae3345faf
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-11 22:28:36 +0530

    Complete product data fetched and displayed

--- day-29/src/components/ScrollIndicators.jsx
+++ day-29/src/components/ScrollIndicators.jsx
@@ -10,7 +10,13 @@ const ScrollIndicators = ({ url }) => {
             setLoading(true);
             const response = await fetch(getUrl)
             const data = await response.json();
-            alert(data)
+
+            if (data && data.products && data.products.length > 0) {
+                setData(data.products);
+                setLoading(false);
+            }
+
+            // alert(data)
             console.log(data);
         } catch (e) {
             console.log(e);
@@ -23,8 +29,11 @@ const ScrollIndicators = ({ url }) => {
     }, [url])
 
     return (
-        <div>
-            <h2>Aman kureshi</h2>
+        <div className='container'>
+            <h1>Custom Scroll Indicator</h1>
+            <div className="data-content">
+                {data && data.length > 0 ? data.map((dataItem) => <p>{dataItem.title}</p>) : null}
+            </div>
         </div>
     )
 }
