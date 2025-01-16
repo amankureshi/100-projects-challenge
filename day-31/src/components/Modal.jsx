688ab787dd6240d2160160b8b807553045da5ec5
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-16 10:20:10 +0530

    Added reusable Modal component with customizable header, body, and footer

--- day-31/src/components/Modal.jsx
+++ day-31/src/components/Modal.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
-const Modal = () => {
-  return <div></div>;
+const Modal = ({ id, header, body, footer }) => {
+  return (
+    <div id={id || "modal"} className="modal">
+      <div className="modal-content">
+        <div className="header">
+          <span className="close-modal-icon">&times;</span>
+          <h2>{header ? header : "Header"}</h2>
+        </div>
+        <div className="body">
+          {body ? (
+            body
+          ) : (
+            <div>
+              <p>This if out Modal</p>
+            </div>
+          )}
+        </div>
+        <div className="footer">{footer ? footer : <h2>Footer</h2>}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
--- day-31/src/components/ModalTest.jsx
+++ day-31/src/components/ModalTest.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
+import Modal from "./Modal";
+import "./Modal.css";
 const ModalTest = () => {
   const [showModalpPopup, setShowModalPopup] = useState(false);
   function handleToggleModal() {
     setShowModalPopup(!showModalpPopup);
-    console.log(showModalpPopup);
+    // console.log(showModalpPopup);
   }
   return (
     <>
       <button onClick={handleToggleModal}>Open modal popup</button>
+      {showModalpPopup && <Modal />}
     </>
   );
 };
