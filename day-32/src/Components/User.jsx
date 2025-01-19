8b6f61b3bb4218452ec2f34d77b6cd58a6d7bcb4
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-19 21:55:24 +0530

    Add user profile details (public repos, followers, following)

--- day-32/src/Components/User.jsx
+++ day-32/src/Components/User.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const User = ({ user }) => {
-  const { avatar_url, followers, following, public_repos, name, login } = user;
+  const {
+    avatar_url,
+    followers,
+    following,
+    public_repos,
+    name,
+    login,
+    created_at,
+  } = user;
+
+  const createDate = new Date(created_at);
 
   return (
     <div className="user-container">
@@ -11,6 +21,24 @@ const User = ({ user }) => {
       </div>
       <div>
         <a href={`https://github.com/${login}`}>{name || login}</a>
+        <p>
+          user joined on{" "}
+          {`${createDate.getDate()}${createDate.toLocaleString("en-us", {
+            month: "short",
+          })} ${createDate.getFullYear()}`}
+        </p>
+      </div>
+      <div className="last-content">
+        <h1>Public Repos</h1>
+        <p>{public_repos}</p>
+      </div>
+      <div className="last-content">
+        <h1>Followers</h1>
+        <p>{followers}</p>
+      </div>
+      <div className="last-content">
+        <h1>Following</h1>
+        <p>{following}</p>
       </div>
     </div>
   );
