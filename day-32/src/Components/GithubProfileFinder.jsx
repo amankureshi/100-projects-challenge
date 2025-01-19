32dfbd708ec131ededf385c6094d1fb34e7f9bfa
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-19 15:22:12 +0530

    Add fetch function for GitHub user data

--- day-32/src/Components/GithubProfileFinder.jsx
+++ day-32/src/Components/GithubProfileFinder.jsx
@@ -3,10 +3,16 @@ import React, { useEffect, useState } from "react";
 const GithubProfileFinder = () => {
   const [userName, setUserName] = useState("amankureshi");
 
+  async function fetchGithubUserData() {
+    const res = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await res.json();
+    console.log(data);
+  }
+
   function handleSubmit() {}
 
   useEffect(() => {
-    fetcGithubUserData();
+    fetchGithubUserData();
   }, []);
 
   return (
