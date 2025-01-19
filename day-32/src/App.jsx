97b31471e03288ae3910548b79c67029f9541b36
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-19 16:39:55 +0530

    createuser component

--- day-32/src/App.jsx
+++ day-32/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import GithubProfileFinder from "./Components/GithubProfileFinder";
+import User from "./Components/User";
 
 function App() {
   return (
     <>
       <GithubProfileFinder />
+      <User />
     </>
   );
 }
---
+++ day-32/src/Components/User.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = (user) => {
+  return <h1>User Components</h1>;
+};
+
+export default User;
