e78a81430542a6a7e806b37b1d2c84e20c0afc65
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-21 12:03:17 +0530

    Added API call to fetch user data for searchautocomplate

--- day-33/src/components/SearchAutoComplate.jsx
+++ day-33/src/components/SearchAutoComplate.jsx
@@ -4,6 +4,21 @@ const SearchAutoComplate = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
 
+  async function fetchListOfUsers() {
+    try {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
   return (
     <div>
       <h1>Search</h1>
