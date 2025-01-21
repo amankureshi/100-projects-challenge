a1d26c2085289ca4a2549dbe1b1c566be9676056
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-21 12:46:51 +0530

    Add search functionality with filtered user suggestions

--- day-33/src/components/SearchAutoComplate.jsx
+++ day-33/src/components/SearchAutoComplate.jsx
@@ -3,13 +3,39 @@ import React, { useEffect, useState } from "react";
 const SearchAutoComplate = () => {
   const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+  const [searchParam, setSearchParam] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filterdUsers, setFilterdUsers] = useState([]);
+
+  function handleChange(event) {
+    const query = event.target.value.toLowerCase();
+    setSearchParam(query);
+    if (query > 1) {
+      const filterData =
+        users && users.length
+          ? users.filter((item) => item.toLowerCase().indexOf(query) > -1)
+          : [];
+      setFilterdUsers(filterData);
+      setShowDropdown(true);
+    } else {
+      showDropdown(false);
+    }
+  }
 
   async function fetchListOfUsers() {
     try {
+      setLoading(true);
       const response = await fetch("https://dummyjson.com/users");
       const data = await response.json();
       console.log(data);
+      if (data && data.users && data.users.length) {
+        setUsers(data.users.map((userItem) => userItem.firstName));
+        setLoading(false);
+        setError(null);
+      }
     } catch (error) {
+      setLoading(false);
       console.log(error);
       setError(error);
     }
@@ -19,10 +45,18 @@ const SearchAutoComplate = () => {
     fetchListOfUsers();
   }, []);
 
+  console.log(users, filterdUsers);
+
   return (
     <div>
       <h1>Search</h1>
-      <input type="text" name="search-user" placeholder="Search here" />
+      <input
+        value={searchParam}
+        onChange={handleChange}
+        type="text"
+        name="search-user"
+        placeholder="Search here"
+      />
     </div>
   );
 };
