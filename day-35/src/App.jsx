986e1d47d17cfdc3d28878444e8a83832ffdb527
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-25 21:49:50 +0530

    Create scrollToSection component

---
+++ day-35/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ScrollToSection from "./assets/components/ScrollToSection";
+
+function App() {
+  return (
+    <>
+      <ScrollToSection />
+    </>
+  );
+}
+
+export default App;
---
+++ day-35/src/assets/components/ScrollToSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ScrollToSection = () => {
+  return (
+    <div>
+      <h1>Scroll to a particular section</h1>
+    </div>
+  );
+};
+
+export default ScrollToSection;
