34733a266824df8b73003ac4802a03c66fb7714d
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-27 23:58:55 +0530

    Added email validation to the form

--- day-36/src/App.tsx
+++ day-36/src/App.tsx
@@ -7,6 +7,19 @@ function App() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  const validationForm = () => {
+    let error = "";
+    let password = "";
+
+
+    if (email === "") {
+      emailError = "Please enter email";
+    }
+    else if (!email.includes("@")) {
+      emailError = "Please include @ in the email";
+    }
+  }
+
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -23,7 +36,8 @@ function App() {
               <form className="space-y-4 md:space-y-6" action="#">
                 <div>
                   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" />
+                  <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" onChange={(e) => setEmail(e, EventTarget, value)} />
+                  {error.email && <p style={{ color: "red", fontSize: "14px" }}>{error.email}</p>}
                 </div>
                 <div>
                   <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -51,8 +65,8 @@ function App() {
               </form>
             </div>
           </div>
-        </div>
-      </section>
+        </div >
+      </section >
     </>
   )
 }
