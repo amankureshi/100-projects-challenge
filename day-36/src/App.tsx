a0dd1079f6b337867b304d0ba6e144f179a44857
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-28 11:28:13 +0530

    password input valdition completed

--- day-36/src/App.tsx
+++ day-36/src/App.tsx
@@ -1,42 +1,48 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState({ email: "", password: "" });
-
 
   const validationForm = () => {
     let emailError = "";
     let passwordError = "";
 
-
     if (email === "") {
       emailError = "Please enter email";
-    }
-    else if (!email.includes("@")) {
+    } else if (!email.includes("@")) {
       emailError = "Please include @ in the email";
     }
-    setError({ email: emailError })
+    if (password === "") {
+      passwordError = "Please enter password";
+    } else if (password.length < 3) {
+      passwordError = "Password must be least 6 characters";
+    }
+    setError({ email: emailError, password: passwordError });
     return !emailError && passwordError;
-
-  }
+  };
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     if (validationForm()) {
-      console.log('Form is valid');
+      console.log("Form is valid");
     }
-  }
-
+  };
 
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <img className="w-13 mr-2" src="https://img.icons8.com/?size=200&id=7819&format=png&color=EBEBEB" alt="logo" />
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              className="w-13 mr-2"
+              src="https://img.icons8.com/?size=200&id=7819&format=png&color=EBEBEB"
+              alt="logo"
+            />
             Jois Us
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -44,42 +50,98 @@ function App() {
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Sign in to your account
               </h1>
-              <form className="space-y-4 md:space-y-6" action="" onSubmit={handleSubmit}>
+              <form
+                className="space-y-4 md:space-y-6"
+                action=""
+                onSubmit={handleSubmit}
+              >
                 <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="text" value={email} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
-                  {error.email && <p style={{ color: "red", fontSize: "14px" }}>{error.email}</p>}
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Your email
+                  </label>
+                  <input
+                    type="text"
+                    value={email}
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter your email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  {error.email && (
+                    <p style={{ color: "red", fontSize: "14px" }}>
+                      {error.email}
+                    </p>
+                  )}
                 </div>
                 <div>
-                  <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                  <label
+                    htmlFor="password"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  {error.password && (
+                    <p style={{ color: "red", fontSize: "14px" }}>
+                      {error.password}
+                    </p>
+                  )}
                 </div>
                 <div className="flex items-center justify-between">
                   <div className="flex items-start">
                     <div className="flex items-center h-5">
-                      <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      />
                     </div>
                     <div className="ml-3 text-sm">
-                      <label className="text-gray-500 dark:text-gray-300">Remember me</label>
+                      <label className="text-gray-500 dark:text-gray-300">
+                        Remember me
+                      </label>
                     </div>
                   </div>
                   {/* <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a> */}
-                  <a href="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
-
+                  <a
+                    href="#"
+                    className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    Forgot password?
+                  </a>
                 </div>
-                <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <button
+                  type="submit"
+                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
                   Sign in
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                  Don’t have an account yet?{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                  >
+                    Sign up
+                  </a>
                 </p>
               </form>
             </div>
           </div>
-        </div >
-      </section >
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
