8721c7d22e06d975092f2a733803709d5764af8b
Aman Kureshi <amankureshi78692@gmail.com>
2025-01-29 15:17:32 +0530

    Implement a loader and logout logic

--- day-36/src/App.tsx
+++ day-36/src/App.tsx
@@ -80,116 +80,153 @@ function App() {
       }
     }
   }
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setLoggedIn(false);
+    setUserInfo({});
+  }
 
 
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a
-            href="#"
-            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
-          >
-            <img
-              className="w-13 mr-2"
-              src="https://img.icons8.com/?size=200&id=7819&format=png&color=EBEBEB"
-              alt="logo"
-            />
-            Jois Us
-          </a>
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
-              </h1>
-              <form
-                className="space-y-4 md:space-y-6"
-                action=""
-                onSubmit={handleSubmit}
+        {pageLoader ? <div>Loading</div> : <>
+          {!loggedIn &&
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+              <a
+                href="#"
+                className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
               >
-                <div>
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                    Your email
-                  </label>
-                  <input
-                    type="text"
-                    value={email}
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Enter your email"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                  {error.email && (
-                    <p style={{ color: "red", fontSize: "14px" }}>
-                      {error.email}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  <label
-                    htmlFor="password"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                <img
+                  className="w-13 mr-2"
+                  src="https://img.icons8.com/?size=200&id=7819&format=png&color=EBEBEB"
+                  alt="logo"
+                />
+                Jois Us
+              </a>
+              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Sign in to your account
+                  </h1>
+                  <form
+                    className="space-y-4 md:space-y-6"
+                    action=""
+                    onSubmit={handleSubmit}
                   >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                  {error.password && (
-                    <p style={{ color: "red", fontSize: "14px" }}>
-                      {error.password}
-                    </p>
-                  )}
-                </div>
-                <div className="flex items-center justify-between">
-                  <div className="flex items-start">
-                    <div className="flex items-center h-5">
-                      <input
-                        id="remember"
-                        aria-describedby="remember"
-                        type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      />
-                    </div>
-                    <div className="ml-3 text-sm">
-                      <label className="text-gray-500 dark:text-gray-300">
-                        Remember me
+                    <div>
+                      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Your email
                       </label>
+                      <input
+                        type="text"
+                        value={email}
+                        name="email"
+                        id="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Enter your email"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      {error.email && (
+                        <p style={{ color: "red", fontSize: "14px" }}>
+                          {error.email}
+                        </p>
+                      )}
                     </div>
-                  </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-                  >
-                    Forgot password?
-                  </a>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
+                      {error.password && (
+                        <p style={{ color: "red", fontSize: "14px" }}>
+                          {error.password}
+                        </p>
+                      )}
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                          <input
+                            id="remember"
+                            aria-describedby="remember"
+                            type="checkbox"
+                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                          />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label className="text-gray-500 dark:text-gray-300">
+                            Remember me
+                          </label>
+                        </div>
+                      </div>
+                      <a
+                        href="#"
+                        className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                    <button
+                      type="submit"
+                      className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Sign in
+                    </button>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Don’t have an account yet?{" "}
+                      <a
+                        href="#"
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      >
+                        Sign up
+                      </a>
+                    </p>
+                  </form>
                 </div>
-                <button
-                  type="submit"
-                  className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                >
-                  Sign in
-                </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
-                  <a
-                    href="#"
-                    className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Sign up
-                  </a>
-                </p>
-              </form>
+              </div>
+            </div>}
+          {
+            loggedIn && userInfo && <div
+              className="flex justify-center">
+              <div className="w-full  max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+
+                <div className="flex flex-col items-center pb-10">
+                  <img
+                    className="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src="/docs/images/people/profile-picture-3.jpg"
+                    alt="Bonnie image"
+                  />
+                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                    Bonnie Green
+                  </h5>
+                  <span className="text-sm text-gray-500 dark:text-gray-400">
+                    Visual Designer
+                  </span>
+                  <div className="flex mt-4 md:mt-6">
+                    <a
+                      href="#"
+                      className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      Log Out
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          }
+        </>}
       </section>
     </>
   );
