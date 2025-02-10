d4de87fd0498f7854561bb81a951dc0b8b6e2771
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-10 14:56:49 +0530

    Add customizable Button component with default props

---
+++ day-37/src/components/Button.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+function Button({
+  label,
+  backgroundColor = "red",
+  size = "md",
+  handleClick,
+  color = "white",
+}) {
+  let scale = 1;
+  if (size === "sm") scale = 0.75;
+  if (size === "lg") scale = 1.5;
+  const style = {
+    backgroundColor,
+    padding: `${scale * 0.5}rem ${scale * 1}rem`,
+    border: "none",
+    color,
+  };
+  return (
+    <button onClick={handleClick} style={style}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
