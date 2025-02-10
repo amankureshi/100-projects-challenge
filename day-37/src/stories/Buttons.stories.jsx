9992548f2bf966367910ee27919348c964519319
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-10 15:06:49 +0530

    Define args for Red Button story in Storybook

--- day-37/src/stories/Buttons.stories.jsx
+++ day-37/src/stories/Buttons.stories.jsx
@@ -14,3 +14,40 @@ export const Secondary = () => <Button label="Secondary Button" />;
 
 const Template = (args) => <Button {...args} />;
 export const Red = Template.bind({});
+
+Red.args = {
+  backgroundColor: "red",
+  label: "Click Me",
+  size: "md",
+  color: "white",
+};
+
+export const Green = Template.bind({});
+Green.args = {
+  backgroundColor: "rebeccapurple",
+  label: "Click Me",
+  size: "md",
+  color: "white",
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  backgroundColor: "yellow",
+  label: "Click Me",
+  size: "sm",
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  backgroundColor: "green",
+  label: "Click Me",
+  size: "lg",
+};
+
+export const LongLabel = Template.bind({});
+LongLabel.args = {
+  backgroundColor: "pink",
+  label: "Long lable last",
+  size: "md",
+  color: "black",
+};
