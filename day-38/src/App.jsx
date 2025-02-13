d86ae87398458149de77a00b3e97b781bd3b57b8
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-13 10:29:15 +0530

    Added state varibles for password input and validation message

--- day-38/src/App.jsx
+++ day-38/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState();
   return (
     <>
       <main className="d-flex justify-content-center align-items-center min-vh-100">
         <div className="container" style={{ width: "26rem" }}>
           <h2>Password Validation</h2>
-          <form>
+          <form onSubmit={handleValidation}>
             <div class="mb-3">
               <label for="formGroupExampleInput" class="form-label">
                 Password
