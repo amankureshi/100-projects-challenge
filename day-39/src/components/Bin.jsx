80187018c4b41011e9c716ed09b53c2aa6f488f3
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-17 00:00:20 +0530

    Fix rendering and drag functionality for Note component

--- day-39/src/components/Bin.jsx
+++ day-39/src/components/Bin.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import { useDrop } from "react-dnd";
 
 const Bin = () => {
-  const [{ canDrop, isOver }, drop] = useDrop(() => ({
-    accept: "note",
-    drop: () => ({ name: "the bin" }),
-    collect: (monitor) => ({
-      isOver: !!monitor.isOver(),
-      canDrop: !!monitor.canDrop(),
+  const [{ canDrop, isOver }, drop] = useDrop(
+    () => ({
+      accept: "note",
+      drop: () => ({ name: "the bin" }),
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+        canDrop: !!monitor.canDrop(),
+      }),
     }),
-  }));
+    []
+  );
 
   return (
-    <div>
+    <div className="border h-48" ref={drop}>
       <h2 className="text-cetner font-medium py-2">Bin</h2>
     </div>
   );
--- day-39/src/components/Note.jsx
+++ day-39/src/components/Note.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
-function Note({ note }) {
+const Note = ({ note }) => {
+  const [{ isDragging }, drag] = useDrag(
+    () => ({
+      type: "note",
+      item: { name: note },
+      end: (item, monitor) => {
+        const dropResult = monitor.getDropResult();
+        if (item && dropResult) {
+          alert(`You threw ${item.name} into ${dropResult.name}`);
+        }
+      },
+      colletct: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    }),
+    []
+  );
+
   return <div>{note}</div>;
-}
+};
 
 export default Note;
