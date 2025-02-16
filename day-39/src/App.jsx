8451320a146d0fa1adc886bf181c6279f159577d
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-16 22:36:11 +0530

    Add localStorage functionality to store and load notes

--- day-39/src/App.jsx
+++ day-39/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import Bin from "./components/Bin";
+import Note from "./components/Note";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [binnedItems, setBinnedItems] = useState([]);
+
+  useEffect(() => {
+    const initialNotes = ["Note 1", "Note 2", "Note 3"];
+    localStorage.setItem("noteList", JSON.stringify(initialNotes));
+
+    let array = localStorage.getItem("noteList");
+    setNotes(JSON.parse(array));
+  }, []);
+
+  console.log(notes);
+
   return (
     <>
       <DndProvider backend={HTML5Backend}>
         <h1 className="text-3xl font-bold text-center">Aman Kureshi</h1>
+        <Bin />
+        <Note />
       </DndProvider>
     </>
   );
