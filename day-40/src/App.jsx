2ad4589c731992ff16852b8a596c8f49750f096e
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-20 22:28:43 +0530

    Add emoji selection and copy to clipboard functionality

--- day-40/src/App.jsx
+++ day-40/src/App.jsx
@@ -1,9 +1,21 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [chooseEmoji, setChooseEmoji] = useState();
+
+  const copyEmojiFuction = (text) => {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+  };
+
   return (
     <>
-      <h1>Project 40</h1>
+      <h1>Emoji</h1>
     </>
   );
 }
