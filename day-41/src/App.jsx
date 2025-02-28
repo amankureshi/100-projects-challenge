2537e57cde0c2f62d88942d3eef4ee59e4574774
Aman Kureshi <amankureshi78692@gmail.com>
2025-02-28 18:18:54 +0530

    create a photoGallery component

--- day-41/src/App.jsx
+++ day-41/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import Introduction from "./Components/Introduction";
-import { BrowserRouter, Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import PhotoGallery from "./Components/PhotoGallery";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Introduction />
+      <Routes>
+        <Route path="/" element={<Introduction />} />
+        <Route path="/gallery" element={<PhotoGallery />} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ day-41/src/Components/PhotoGallery.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const PhotoGallery = () => {
+  const photos = [
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122102942/1a-(2).jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122102942/2b.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103211/13.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103211/15.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103210/12.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103210/14.jpg",
+  ];
+
+  return (
+    <div>
+      <div className="photo-gallery">
+        {photos.map((photo, index) => (
+          <Link to={`/photo/${index}`} key={index}>
+            <img src={photo} alt={`Photo ${index + 1}`} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoGallery;
