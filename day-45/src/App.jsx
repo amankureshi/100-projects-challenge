822a08a3d2bc763fc24650d9792e870ae57f1b1a
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-08 22:23:57 +0530

    Added useState in Menu component

--- day-45/src/App.jsx
+++ day-45/src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
+import Menu from "./Menu";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [brush, setBrushColor] = useState();
+  const [brushWidth, setBrushWidth] = useState();
+  const [brushOpacity, setBurshOpacity] = useState();
   return (
     <>
       <h1>Aman Kureshi</h1>
+      <div className="drow-area">
+        <Menu
+          setBrush={setBrushColor}
+          setBurshOpacity={setBurshOpacity}
+          setBrushWidth={setBrushWidth}
+        />
+      </div>
     </>
   );
 }
