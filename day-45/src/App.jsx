b9d40ffa62b72372d5eded3c07c847b4224a491e
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-09 15:01:48 +0530

    Added canvas contect configuration  with dynamic brush settings

--- day-45/src/App.jsx
+++ day-45/src/App.jsx
@@ -1,13 +1,28 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Menu from "./Menu";
 import "./App.css";
 
 function App() {
-  const [brush, setBrushColor] = useState("green");
+  const [brushColor, setBrushColor] = useState("green");
   const [brushWidth, setBrushWidth] = useState(8);
   const [brushOpacity, setBurshOpacity] = useState(0.5);
   const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("wd");
+    ctx.lineCap = "round";
+    ctx.lineJoine = "round ";
+    ctx.globalAlpha = brushOpacity;
+    ctx.strokeStyle = brushColor;
+    ctx.lineWidth = brushWidth;
+    ctxRef.current = ctx;
+  }, [brushColor, brushOpacity, brushWidth]);
+
+  const startDraw = (e) => {};
+
+  const endDraw = (e) => {};
   return (
     <>
       <h1>Paint App</h1>
