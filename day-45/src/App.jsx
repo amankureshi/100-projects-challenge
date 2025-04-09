77fa423889c14b89d2aea9f3d4320a541acc6688
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-09 14:42:15 +0530

    Added canvas and update value in useState

--- day-45/src/App.jsx
+++ day-45/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Menu from "./Menu";
 import "./App.css";
 
 function App() {
-  const [brush, setBrushColor] = useState();
-  const [brushWidth, setBrushWidth] = useState();
-  const [brushOpacity, setBurshOpacity] = useState();
+  const [brush, setBrushColor] = useState("green");
+  const [brushWidth, setBrushWidth] = useState(8);
+  const [brushOpacity, setBurshOpacity] = useState(0.5);
+  const canvasRef = useRef(null);
+
   return (
     <>
       <h1>Paint App</h1>
@@ -15,6 +17,14 @@ function App() {
           setBurshOpacity={setBurshOpacity}
           setBrushWidth={setBrushWidth}
         />
+        <canvas
+          width={"1200px"}
+          height={"500px"}
+          ref={canvasRef}
+          onMouseDown={startDraw}
+          onMouseUp={endDraw}
+          onMouseMove={draw}
+        ></canvas>
       </div>
     </>
   );
