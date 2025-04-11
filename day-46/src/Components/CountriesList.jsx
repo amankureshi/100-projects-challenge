a07c5bc60874a646c7f97be677cf39568bf0fbbd
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-11 23:40:25 +0530

    api added

--- day-46/src/Components/CountriesList.jsx
+++ day-46/src/Components/CountriesList.jsx
@@ -2,15 +2,23 @@ import React, { useEffect, useState } from "react";
 import CountrieaShimmer from "./CountriesListShimmer";
 
 const CountriesList = ({ query }) => {
+  const [countriesData, setCountriesData] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountriesData(data);
+      });
+  }, []);
+  console.log(data);
+  if (countriesData.length === 0) {
+    return <CountrieaShimmer />;
+  }
   return (
     <>
-      {!countriesData.length ? (
-        <CountrieaShimmer />
-      ) : (
-        <div className="Countries-container">
-          <h1>Countries List</h1>
-        </div>
-      )}
+      <div className="Countries-container">
+        <h1>Countries List</h1>
+      </div>
     </>
   );
 };
