21e86ad0c4e81a9eb9f34d2ab6c1791482ba4b96
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-12 15:29:07 +0530

    added Introduction component

---
+++ day-46/src/Components/Introduction.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CountriesList from "./CountriesList";
+
+const Introduction = () => {
+  return (
+    <div>
+      <h1>Welcome to the Country App</h1>
+      <CountriesList query="" />
+    </div>
+  );
+};
+
+export default Introduction;
