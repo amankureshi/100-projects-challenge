a83058f516b58a948c9d27171f68db796c46eb4a
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-13 23:24:32 +0530

    add country fetch logic

--- day-46/src/App.jsx
+++ day-46/src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import Introduction from "./Components/Introduction";
 import About from "./Components/About";
+import CountryDetails from "./Components/CountryDetails ";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        <Route path="/:countryName" element={<CountryDetails />} />
         <Route path="/" element={<Introduction />} />
         <Route path="/about" element={<About />} />
       </Routes>
--- day-46/src/Components/CountryDetails .jsx
+++ day-46/src/Components/CountryDetails .jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const CountryDetails = () => {
   const { countryName } = useParams();
+  const [country, setCountry] = useState(null);
 
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setCountry(data[0]);
+      });
+  }, [countryName]);
+
+  if (!country) return <h2>Loading....</h2>;
   return (
     <div>
       <h2>Details for {countryName}</h2>
