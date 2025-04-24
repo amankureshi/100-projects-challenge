5693940c7d8c5b012616d3f93ca8b8d4394917fb
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-24 16:22:36 +0530

    added h1 and text

--- day-47/src/Pages/Home.jsx
+++ day-47/src/Pages/Home.jsx
@@ -10,11 +10,17 @@ const Home = () => {
             <IoSearchSharp size={24} />
             <input
               type="text"
-              className="text-sm md:text-md grow"
+              className="text-sm md:text-md grow "
               placeholder="what do you want to cook today ?"
             />
           </label>
         </form>
+        <h1 className="font-bold text-3cl md:text-5xl mt-4">
+          Recommended Recipes
+        </h1>
+        <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
+          Popular choices
+        </p>
       </div>
     </div>
   );
