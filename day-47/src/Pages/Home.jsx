332e20842288ce2a762437a7d3f6e74af6a34008
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-25 23:33:34 +0530

    text-center

--- day-47/src/Pages/Home.jsx
+++ day-47/src/Pages/Home.jsx
@@ -3,10 +3,10 @@ import { IoSearchSharp } from "react-icons/io5";
 
 const Home = () => {
   return (
-    <div className="bg-[#faf9fb] flex-1 p-10">
+    <div className="bg-[#faf9fb] w-full flex justify-center p-10">
       <div className="max-w-screen-lg mx-auto">
         <form>
-          <label className="input shadow-md flex items-center gap-2">
+          <label className="input shadow-md flex items-center gap-2 w-full">
             <IoSearchSharp size={24} />
             <input
               type="text"
