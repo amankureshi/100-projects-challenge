f3ceee938aafc3eb086b76a7ddd574750e2e5df3
Aman Kureshi <amankureshi78692@gmail.com>
2025-04-26 21:21:13 +0530

    added image and icons in Home.jsx

--- day-47/src/Pages/Home.jsx
+++ day-47/src/Pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
+import { LuSoup } from "react-icons/lu";
+import { FaRegHeart } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -21,6 +23,26 @@ const Home = () => {
         <p className="text-slate-500 font-semibold ml-1 my-2 text-sm tracking-tight">
           Popular choices
         </p>
+        <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+          <div className="flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative">
+            <a href="" className="relative h-33">
+              <img
+                src="https://cdn.pixabay.com/photo/2023/09/25/22/08/ai-generated-8276129_1280.jpg"
+                alt="recipe-image"
+                className="rounded-md w-full h-full object-cover cursor-pointer"
+              />
+              <div className="absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm">
+                <LuSoup size={18} />4 Searving
+              </div>
+              <div className="absolute top-1 right-2 bg-white rounded-full p-20 cursor-pointer">
+                <FaRegHeart
+                  size={"20"}
+                  className="hover:fill-red-500 hover:text-red-500"
+                />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
