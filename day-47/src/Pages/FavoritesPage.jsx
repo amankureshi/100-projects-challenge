3ddd4e0793c54ac25e4c7afbaf6a541f97365a36
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-02 23:56:34 +0530

    update in FavoritesPage component and added image an 404-image

--- day-47/src/Pages/FavoritesPage.jsx
+++ day-47/src/Pages/FavoritesPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
 const FavoritesPage = () => {
+  const fav = false;
   return (
-    <div>
-      <h2>Favorites</h2>
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen">
+        <p className="font-bold text-3xl md:text-5xl my-4">Favorites</p>
+        {!fav && (
+          <div className="h-[80vh] flex flex-col items-center justify-c gap-4 ">
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/026/573/373/original/rain-umbrella-404-error-animation-protection-weather-rainy-error-message-gif-motion-graphic-autumn-umbrella-raindrops-falling-animated-cartoon-line-object-4k-isolated-on-white-background-video.jpg"
+              alt="404-error-image"
+              className="h-5/5"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
