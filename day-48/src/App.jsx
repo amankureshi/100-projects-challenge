ead46dd5ad2fb897a768a772dc814328f7b52adf
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-15 22:34:29 +0530

    added useState

--- day-48/src/App.jsx
+++ day-48/src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
   return (
     <>
       <h1>Aman Kureshi</h1>
