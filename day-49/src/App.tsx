64437dc26be7cf2a56dcd2bd2df4cf33b3d6184f
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-20 23:59:21 +0530

    add and render CountdownTimer component with initial time

--- day-49/src/App.tsx
+++ day-49/src/App.tsx
@@ -3,12 +3,20 @@ import './App.css'
 
 const words = faker.word.words(10)
 const App = () => {
-  return <GenerateWords words={words} />
+  return <>
+    <CountdownTimer timeLeft={30} />
+    <GenerateWords words={words} />
+
+  </>
 }
 const GenerateWords = ({ words }: { words: string }) => {
   return <div className="text-4xl text-center text-slate-500">
     {words}
   </div>
 }
-
+const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
+  return <h2 className="font-medium text-amber-300">
+    Time:{timeLeft}
+  </h2>
+}
 export default App
