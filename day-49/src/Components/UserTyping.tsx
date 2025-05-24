2d72014f240cc662e9a42ea2ac7ab814d341fb61
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-24 21:44:13 +0530

    Extract Character component to render styled characters in UserTyping

--- day-49/src/Components/UserTyping.tsx
+++ day-49/src/Components/UserTyping.tsx
@@ -9,10 +9,14 @@ const UserTyping = () => ({
     return (
         <div className={className}>
             {typedCharacters.map((char, index) => {
-                return <span key={`${char}_${index}`}>{char}</span>
+                return <Character key={`${char}_${index}`} char={char} />
             })}
         </div>
     )
+}
+
+const Character = ({ char }: { char: string }) => {
+    return <span className="text-amber-300">{char}</span>
 }
 
 export default UserTyping
