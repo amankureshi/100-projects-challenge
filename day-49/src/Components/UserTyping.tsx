4641ff5da9d92436d18191c5dae0716a010f0fc2
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-24 21:18:21 +0530

    set up userTyping component structure and split user input into characters

---
+++ day-49/src/Components/UserTyping.tsx
@@ -0,0 +1,16 @@
+const UserTyping = () => ({
+    userInput,
+    className,
+}: {
+    userInput: string;
+    className?: string;
+}) => {
+    const typedCharacters = userInput.split("")
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default UserTyping
