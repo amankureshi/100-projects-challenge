330151baf0e7789c275a752fe5b38054a5863571
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-25 14:34:13 +0530

    Create WordsContainer component and use it to wrap GenerateWords and UserTyping

--- day-49/src/App.tsx
+++ day-49/src/App.tsx
@@ -1,29 +1,45 @@
 import { faker } from "@faker-js/faker";
-import RestartBtn from "./Components/RestartBtn.tsx"
-import './App.css'
+import RestartBtn from "./Components/RestartBtn.tsx";
+import "./App.css";
 import Results from "./Components/Results.tsx";
 import UserTyping from "./Components/UserTyping.tsx";
+import { Children } from "react";
 
-const words = faker.word.words(10)
+const words = faker.word.words(10);
 const App = () => {
-  return <>
-    <CountdownTimer timeLeft={30} />
-    <div className="relative max-w-xl mt-3">
-      <GenerateWords words={words} />
-      <UserTyping className="absolute inset-0" userInput={words} />
+  return (
+    <>
+      <CountdownTimer timeLeft={30} />
+      <WordsContainer>
+        <GenerateWords words={words} />
+        <UserTyping className="absolute inset-0" userInput={"test"} />
+      </WordsContainer>
+      <RestartBtn
+        className={"mx-auto mt-10 text-slate-500"}
+        onRestart={() => null}
+      />
+      <Results
+        className="mt-10"
+        errors={10}
+        accuracyPercentage={100}
+        total={200}
+      />
+    </>
+  );
+};
+
+const WordsContainer = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="relative text-3xl max-w-xl leading-relaxed">
+      {children}
     </div>
-    <RestartBtn className={"mx-auto mt-10 text-slate-500"} onRestart={() => null} />
-    <Results className="mt-10" errors={10} accuracyPercentage={100} total={200} />
-  </>
+  )
 }
+
 const GenerateWords = ({ words }: { words: string }) => {
-  return <div className="text-4xl text-center text-slate-500">
-    {words}
-  </div>
-}
+  return <div className="text-center text-slate-500">{words}</div>;
+};
 const CountdownTimer = ({ timeLeft }: { timeLeft: number }) => {
-  return <h2 className="font-medium text-amber-300">
-    Time:{timeLeft}
-  </h2>
-}
-export default App
+  return <h2 className="font-medium text-amber-300">Time:{timeLeft}</h2>;
+};
+export default App;
