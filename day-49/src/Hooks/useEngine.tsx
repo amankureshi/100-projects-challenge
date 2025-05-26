f56dc9328359de090c7cc168dc141f53bfce0882
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-26 18:12:02 +0530

    Fix type export typo and integrate useWords hook to get words and updater

--- day-49/src/Hooks/useEngine.tsx
+++ day-49/src/Hooks/useEngine.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
+import useWords from "./useWords"
 
-export typr State = "state" | "run" | "finish"
+export type State = "start" | "run" | "finish"
 
 const useEngine = () => {
     const [state, setState] = useState<State>("start")
-    return { state }
+    const { words, updateWords } = useWords(NUMBER_OF_WORDS)
+
+    return { state, words }
 }
 export default useEngine;
