23d48875957e108ebc32d2748f761541d0571652
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-27 15:51:11 +0530

    Add props to Character and useTyping components

--- day-49/src/Components/UserTyping.tsx
+++ day-49/src/Components/UserTyping.tsx
@@ -3,23 +3,26 @@ import Caret from "./Caret";
 const UserTyping = ({
     userInput,
     className,
+    words,
 }: {
     userInput: string;
+    words: string;
     className?: string;
 }) => {
     const typedCharacters = userInput.split("")
+
     return (
         <div className={className}>
             {typedCharacters.map((char, index) => {
-                return <Character key={`${char}_${index}`} char={char} />
+                return <Character key={`${char}_${index}`} actual={char} expected={words[index]} />
             })}
             <Caret />
         </div>
     )
 }
 
-const Character = ({ char }: { char: string }) => {
-    return <span className="text-amber-300">{char}</span>
+const Character = ({ actual, expected }: { actual: string, expected: string }) => {
+    return <span className="text-amber-300">{expected}</span>
 }
 
 export default UserTyping;
