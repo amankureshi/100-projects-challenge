7954f9816f98477d7dadeb0e2bec606a30a244e7
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-27 20:59:13 +0530

    Add error counting logic in useEngine using cursor and typed input

--- day-49/src/Hooks/useEngine.tsx
+++ day-49/src/Hooks/useEngine.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import useWords from "./useWords"
 import useCountdownTimer from "./useCountdownTimer";
 import useTypings from "./useTypings";
+import { countErrors } from "../utils/Helper";
 
 export type State = "start" | "run" | "finish"
 
@@ -15,6 +16,13 @@ const useEngine = () => {
         useCountdownTimer(COUNTDOWN_SECONDS)
     const { typed, cursor, clearTyped, resetTotalTyped, totayTyped } =
         useTypings(state !== 'finish')
+
+    const [errors, setError] = useState(0);
+    const sumErrors = useCallback(() => {
+        const wordsReached = words.substring(0, cursor);
+        setError((prevError) => prevError + countErrors(typed, wordsReached))
+    }, [typed, words, cursor])
+
     return { state, words, timeLeft, typed }
 }
 export default useEngine;
