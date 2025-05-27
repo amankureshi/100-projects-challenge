8170549fc7f4b637d654b25fabff65b5a659f5b2
Aman Kureshi <amankureshi78692@gmail.com>
2025-05-27 22:09:20 +0530

    Handle word completion: update words, clear input, and count errors

--- day-49/src/Hooks/useEngine.tsx
+++ day-49/src/Hooks/useEngine.tsx
@@ -1,33 +1,32 @@
-import { useCallback, useEffect, useState } from "react"
-import useWords from "./useWords"
+import { useCallback, useEffect, useState } from "react";
+import useWords from "./useWords";
 import useCountdownTimer from "./useCountdownTimer";
 import useTypings from "./useTypings";
 import { countErrors } from "../utils/Helper";
 
-export type State = "start" | "run" | "finish"
+export type State = "start" | "run" | "finish";
 
 const NUMBER_OF_WORDS = 12;
 const COUNTDOWN_SECONDS = 30;
 
 const useEngine = () => {
-    const [state, setState] = useState<State>("start")
-    const { words, updateWords } = useWords(NUMBER_OF_WORDS)
+    const [state, setState] = useState<State>("start");
+    const { words, updateWords } = useWords(NUMBER_OF_WORDS);
     const { timeLeft, startCountdown, resetCountdown } =
-        useCountdownTimer(COUNTDOWN_SECONDS)
-    const { typed, cursor, clearTyped, resetTotalTyped, totayTyped } =
-        useTypings(state !== 'finish')
-
-
+        useCountdownTimer(COUNTDOWN_SECONDS);
+    const { typed, cursor, clearTyped, resetTotalTyped, totayTyped } = useTypings(
+        state !== "finish"
+    );
 
     const [errors, setError] = useState(0);
 
     const isStarting = state === "start" && cursor > 0;
+    const areWordsFinished = cursor === words.length;
 
     const sumErrors = useCallback(() => {
         const wordsReached = words.substring(0, cursor);
-        setError((prevError) => prevError + countErrors(typed, wordsReached))
+        setError((prevError) => prevError + countErrors(typed, wordsReached));
     }, [typed, words, cursor]);
-
 
     //As soon the user start typing the first letter, we start
     useEffect(() => {
@@ -37,14 +36,30 @@ const useEngine = () => {
         }
     }, [isStarting, startCountdown, cursor]);
 
-
     //When the time is up, we've finished
     useEffect(() => {
         console.log("time is up....");
         setState("finish");
         sumErrors();
-    }, [timeLeft, sumErrors])
+    }, [timeLeft, sumErrors]);
 
-    return { state, words, timeLeft, typed }
-}
+    useEffect(() => {
+        if (areWordsFinished) {
+            console.log("Words are finished");
+            sumErrors();
+            updateWords();
+            clearTyped();
+        }
+    }, [
+        cursor,
+        words,
+        clearTyped,
+        typed,
+        areWordsFinished,
+        updateWords,
+        sumErrors,
+    ]);
+
+    return { state, words, timeLeft, typed };
+};
 export default useEngine;
