0e0f13f207314118640e8a7b9cd07151280dad79
Aman Kureshi <amankureshi78692@gmail.com>
2025-06-05 23:41:21 +0530

    added day-51 project

---
+++ day-51/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="passbox">
+        <h2>Password Generator</h2>
+        <div className="passwordBoxInput">
+          <input type="text" /> <button>copy</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
