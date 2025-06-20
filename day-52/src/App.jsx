372962d467ed82dd17101dc6d5a717926d5b06c1
Aman Kureshi <amankureshi78692@gmail.com>
2025-06-21 00:19:37 +0530

    added darkMode logic

--- day-52/src/App.jsx
+++ day-52/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 import TextConverter from "./TextConverter";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div>
+    <div className={darkMode ? "app dark-mode" : "app"}>
+      <nav className="navbar">
+        <h2>TextConverter</h2>
+        <label className="switch">
+          <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+          <span className="slider"></span>
+        </label>
+      </nav>
       <TextConverter />
     </div>
   );
