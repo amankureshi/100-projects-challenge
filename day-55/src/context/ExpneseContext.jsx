ef69698fb35937f85f484a6666a76b1bc1ccc997
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-17 17:28:27 +0530

    Implement reducer and context provider for managing expenses

--- day-55/src/context/ExpneseContext.jsx
+++ day-55/src/context/ExpneseContext.jsx
@@ -1,9 +1,32 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 
 const ExpenseContext = createContext();
 
-const ExpneseContext = () => {
-  return <div></div>;
+const initialState = JSON.parse(localStorage.getItem("expenses")) || [];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.payload];
+    case "DELETE":
+      return state.filter((item) => item.id !== action.payload);
+    default:
+      return state;
+  }
 };
 
-export default ExpneseContext;
+const ExpneseProvider = ({ children }) => {
+  const [state, dispath] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(state));
+  }, [state]);
+
+  return (
+    <ExpenseContext.Provider value={{ state, dispath }}>
+      {children}
+    </ExpenseContext.Provider>
+  );
+};
+
+export default { ExpenseContext, ExpneseProvider };
