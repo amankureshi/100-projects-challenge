60432ef32e48faeb00096ec0a89973be84e3f194
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-17 17:11:04 +0530

    added day-55 projects

---
+++ day-55/src/context/ExpneseContext.jsx
@@ -0,0 +1,9 @@
+import React, { createContext } from "react";
+
+const ExpenseContext = createContext();
+
+const ExpneseContext = () => {
+  return <div></div>;
+};
+
+export default ExpneseContext;
