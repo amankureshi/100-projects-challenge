8b97e59ae42946510d0e4bb160176352a7821559
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-17 18:37:41 +0530

    Added form and input

--- day-55/src/Components/AddExpeseForm.jsx
+++ day-55/src/Components/AddExpeseForm.jsx
@@ -6,7 +6,25 @@ const AddExpeseForm = () => {
   const [amount, setAmount] = useState("");
   const { dispatch } = useContext(ExpneseContext);
 
-  return <div></div>;
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="Expense Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        required
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
 };
 
 export default AddExpeseForm;
