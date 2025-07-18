11084a004e563b2605b4cdc146bc7748136a77b6
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-18 22:21:19 +0530

    added wallet image

--- day-55/src/App.jsx
+++ day-55/src/App.jsx
@@ -1,16 +1,15 @@
 import ExpenseList from "./components/ExpenseList";
 import { ExpenseProvider } from "./context/ExpenseContext";
 import AddExpenseForm from "./Components/AddExpenseForm";
-import { IoWalletSharp } from "react-icons/io5";
-
+import walletImage from "./assets/wallet-img.png";
 import "./App.css";
 function App() {
   return (
     <ExpenseProvider>
       <div className="min-h-screen flex items-center justify-center hero-bg">
         <div className="bg-[#00023B] p-6 rounded-xl shadow-md w-full max-w-md text-white">
-          <h1 className="text-2xl  text-center text-white mb-4 flex items-center justify-center">
-            <IoWalletSharp className="text-5xl" />
+          <h1 className="text-2xl  text-center text-white mb-4 flex items-center justify-center ">
+            <img src={walletImage} alt="wallet" width={100} />
             My Budget Tracker
           </h1>
           <AddExpenseForm />
