08533342b42cbe9fe2c2a0f83af128da95f89388
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-19 10:55:37 +0530

    text change

--- day-55/src/App.jsx
+++ day-55/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <ExpenseProvider>
       <div className="min-h-screen flex items-center justify-center hero-bg px-4 sm:px-6">
         <div className="bg-[#00023B] p-4 sm:p-6 rounded-xl shadow-md w-full max-w-md text-white">
-          <h1 className="text-xl sm:text-2xl  text-center text-white mb-4 flex flex-col sm:flex-row items-center justify-center">
+          <h1 className="text-2xl sm:text-2xl  text-center text-white mb-4 flex flex-col sm:flex-row items-center justify-center">
             <img src={walletImage} alt="wallet" width={100} />
             My Budget Tracker
           </h1>
