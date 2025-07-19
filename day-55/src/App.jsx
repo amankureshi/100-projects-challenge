7ddefc1877adfbf28a6a9732253541509102885e
Aman Kureshi <amankureshi78692@gmail.com>
2025-07-19 10:48:38 +0530

    Made a responsive App.jsx file

--- day-55/src/App.jsx
+++ day-55/src/App.jsx
@@ -1,4 +1,4 @@
-import ExpenseList from "./components/ExpenseList";
+import ExpenseList from "./Components/ExpenseList";
 import { ExpenseProvider } from "./context/ExpenseContext";
 import AddExpenseForm from "./Components/AddExpenseForm";
 import walletImage from "./assets/wallet-img.png";
@@ -6,9 +6,9 @@ import "./App.css";
 function App() {
   return (
     <ExpenseProvider>
-      <div className="min-h-screen flex items-center justify-center hero-bg">
-        <div className="bg-[#00023B] p-6 rounded-xl shadow-md w-full max-w-md text-white">
-          <h1 className="text-2xl  text-center text-white mb-4 flex items-center justify-center ">
+      <div className="min-h-screen flex items-center justify-center hero-bg px-4 sm:px-6">
+        <div className="bg-[#00023B] p-4 sm:p-6 rounded-xl shadow-md w-full max-w-md text-white">
+          <h1 className="text-xl sm:text-2xl  text-center text-white mb-4 flex flex-col sm:flex-row items-center justify-center">
             <img src={walletImage} alt="wallet" width={100} />
             My Budget Tracker
           </h1>
