abfcc203a555cb37b77279c534826bf10bb9cd49
Aman kureshi <amankureshi78692@gmail.com>
2025-07-28 20:07:48 +0530

    added useState

--- day-56/src/Components/NoteForm.jsx
+++ day-56/src/Components/NoteForm.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NoteForm = () => {
+  const [note, setNote] = useState("");
   return (
     <div>
       <h2>NoteForm</h2>
       <form>
-        <input type="text" placeholder="write a new note" />
+        <input type="text" placeholder="write a new note" value={note} />
       </form>
       <button>Add Note</button>
     </div>
