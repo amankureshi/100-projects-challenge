ee722e7edf4218c581e4b8fc224ca9d74099dd09
Aman kureshi <amankureshi78692@gmail.com>
2025-07-28 20:25:19 +0530

    added Onchange event,HandleChange function

--- day-56/src/Components/NoteForm.jsx
+++ day-56/src/Components/NoteForm.jsx
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 
 const NoteForm = () => {
   const [note, setNote] = useState("");
+  console.log(note);
+
+  const handleChange = (e) => {
+    setNote(e.target.value);
+  };
+
   return (
     <div>
       <h2>NoteForm</h2>
       <form>
-        <input type="text" placeholder="write a new note" value={note} />
+        <input
+          type="text"
+          placeholder="write a new note"
+          value={note}
+          onChange={handleChange}
+        />
       </form>
       <button>Add Note</button>
     </div>
