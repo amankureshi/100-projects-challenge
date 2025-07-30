1c834a357e18bebaef67e6fd5590bf2ead4f2f0c
Aman kureshi <amankureshi78692@gmail.com>
2025-07-30 23:38:20 +0530

    data store in localStorage added this logic

--- day-56/src/App.jsx
+++ day-56/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import NoteForm from "./Components/NoteForm";
 import NoteList from "./Components/NoteList";
@@ -7,6 +7,11 @@ function App() {
   const [notes, setNotes] = useState([]);
   console.log(notes);
 
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem("note")) || [];
+    setNotes(savedNotes);
+  }, []);
+
   const addNote = (newNote) => {
     const updateNotes = [...notes, newNote];
     setNotes(updateNotes);
@@ -14,7 +19,7 @@ function App() {
   };
 
   const deleteNote = (index) => {
-    const updateNotes = notes.filter((_) => i !== index);
+    const updateNotes = notes.filter((_, i) => i !== index);
     setNotes(updateNotes);
     localStorage.setItem("note", JSON.stringify(updateNotes));
   };
