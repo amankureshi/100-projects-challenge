9958965764940e16e209756dc2db2f00d5670ff5
Aman kureshi <amankureshi78692@gmail.com>
2025-08-08 19:08:41 +0530

    added addTransaction function and create a object

--- day-57/src/App.jsx
+++ day-57/src/App.jsx
@@ -2,9 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [tranction, setTraction] = useState([]);
+  const [transaction, setTraction] = useState([]);
   const [desc, setDesc] = useState("");
   const [amount, setAmount] = useState("");
+
+  const addTransaction = (e) => {
+    e.preventDefault();
+    if (!desc || !amount) return;
+    const newTransation = {
+      id: Date.now(),
+      desc,
+      amount: parseFloat(amount),
+    };
+    setTraction([newTransation, ...transaction]);
+    setDesc("");
+    setAmount("");
+  };
 
   return (
     <>
