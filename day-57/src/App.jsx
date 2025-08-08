0884fa541a2d10b9878024427d7d153e748ab933
Aman kureshi <amankureshi78692@gmail.com>
2025-08-08 07:13:18 +0530

    added form code

--- day-57/src/App.jsx
+++ day-57/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <form action=""></form>
+      <form>
+        <input type="text" placeholder="Discription" />
+        <input type="number" placeholder="Amount" />
+        <button>Add Transation</button>
+      </form>
     </>
   );
 }
