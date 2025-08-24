8c49c16021857d055fb8b781d94aeae15a9918ff
Aman kureshi <amankureshi78692@gmail.com>
2025-08-24 22:37:20 +0530

    added day-58 project

---
+++ day-58/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h2 className="text-center">Image Gallery</h2>
+        <div className="row g-3"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
