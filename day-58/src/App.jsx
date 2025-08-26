f5798d32e0fcaf6fafdfb06d5d2e6b75145b1644
Aman kureshi <amankureshi78692@gmail.com>
2025-08-26 15:00:52 +0530

    add closeLightbox and nextImage functions for Lightbox functionality

--- day-58/src/App.jsx
+++ day-58/src/App.jsx
@@ -5,13 +5,26 @@ function App() {
   const [selectedImg, setSelectedImg] = useState(null);
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const openLightBox = (index) => {
+    setSelectedImg(images[index]);
+    setCurrentIndex(index);
+  };
+
+  const closeLightbox = () => setSelectedImg(null);
+
+  const nextImage = () => {
+    const newIndex = (currentIndex + 1) % images.length;
+    setCurrentIndex(newIndex);
+    setSelectedImg(image[newIndex]);
+  };
+
   return (
     <>
       <div className="container">
         <h2 className="text-center">Image Gallery</h2>
         <div className="row g-3">
           <div className="col-md-4">
-            <img src="" alt="image" className="img-fluid" />
+            <img src="pending-image" alt="image" className="img-fluid" />
             <button className="btn-primary">About me</button>
           </div>
         </div>
@@ -19,7 +32,7 @@ function App() {
       <div className="modal fade">
         <div className="d-flex btn-light">
           <button className="btn">Prev</button>
-          <img src="" alt="selected-image" />
+          <img src="pending-image" alt="selected-image" />
           <button className="btn">Next</button>
         </div>
         <button className="btn">X</button>
