91532ef326bfc414a11a17bf8f108ed3fa67236a
Aman kureshi <amankureshi78692@gmail.com>
2025-08-26 15:10:17 +0530

    apply a map on image and condition apply on buttons

--- day-58/src/App.jsx
+++ day-58/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   const nextImage = () => {
     const newIndex = (currentIndex + 1) % images.length;
     setCurrentIndex(newIndex);
-    setSelectedImg(image[newIndex]);
+    setSelectedImg(images[newIndex]);
   };
 
   return (
@@ -31,19 +31,28 @@ function App() {
       <div className="container">
         <h2 className="text-center">Image Gallery</h2>
         <div className="row g-3">
-          <div className="col-md-4">
-            <img src="pending-image" alt="image" className="img-fluid" />
-            <button className="btn-primary">About me</button>
+          {images.map((img, i) => {
+            <div className="col-md-4">
+              <img
+                src={img}
+                alt={`Gallery ${i}`}
+                className="img-fluid"
+                onClick={() => openLightBox(i)}
+              />
+              <button className="btn-primary">About me</button>
+            </div>;
+          })}
+        </div>
+        {selectedImg && (
+          <div className="modal fade">
+            <div className="d-flex btn-light">
+              <button className="btn">Prev</button>
+              <img src="pending-image" alt="selected-image" />
+              <button className="btn">Next</button>
+            </div>
+            <button className="btn">X</button>
           </div>
-        </div>
-      </div>
-      <div className="modal fade">
-        <div className="d-flex btn-light">
-          <button className="btn">Prev</button>
-          <img src="pending-image" alt="selected-image" />
-          <button className="btn">Next</button>
-        </div>
-        <button className="btn">X</button>
+        )}
       </div>
     </>
   );
