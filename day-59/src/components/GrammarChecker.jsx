9b1b7e8fe89aeb759da9c29b31b7736bd2b44c33
Aman kureshi <amankureshi78692@gmail.com>
2025-09-12 18:36:58 +0530

    added onChange event in textarea and two buttons

--- day-59/src/components/GrammarChecker.jsx
+++ day-59/src/components/GrammarChecker.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import SuggestionCard from "./SuggestionCard";
+import { runChecks } from "../utils/Cheack";
 
 const GrammarChecker = () => {
   const [text, setText] = useState("");
@@ -18,12 +20,21 @@ const GrammarChecker = () => {
       <h2 h2 className="text-2xl font-bold mb-4">
         GrammarChecker Checker
       </h2>
-      <textarea className="w-full h-40 p-3 border rounded mb-3 bg-white" />
+      <textarea
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Type or paste  your paragraph"
+        className="w-full h-40 p-3 border rounded mb-3 bg-white"
+      />
       <div className="flex gap-3 mb-4">
-        <button className="px-4 py-2 rounded bg-blue-600 text-white">
+        <button
+          onClick={handleCheck}
+          className="px-4 py-2 rounded bg-blue-600 text-white"
+        >
           Check
         </button>
-        <button className="px-4 py-2 rounded border ">Clear</button>
+        <button onClick={handleCheck} className="px-4 py-2 rounded border ">
+          Clear
+        </button>
       </div>
     </div>
   );
