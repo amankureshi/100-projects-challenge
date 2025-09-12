f663af9b8a81a50e6438348da2b024f3686eaeae
Aman kureshi <amankureshi78692@gmail.com>
2025-09-12 18:16:13 +0530

    added textarea

--- day-59/src/components/GrammarChecker.jsx
+++ day-59/src/components/GrammarChecker.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const GrammarChecker = () => {
-  return <div>GrammarChecker</div>;
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h2 h2 className="text-2xl font-bold mb-4">
+        GrammarChecker Checker
+      </h2>
+      <textarea className="w-full h-40 p-3 border rounded mb-3 bg-white" />
+    </div>
+  );
 };
 
 export default GrammarChecker;
