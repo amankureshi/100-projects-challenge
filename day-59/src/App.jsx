d1178c26ed4a4017070c3c64c184e0da437ed56a
Aman kureshi <amankureshi78692@gmail.com>
2025-09-17 19:42:48 +0530

    added grammer image

--- day-59/src/App.jsx
+++ day-59/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import GrammarChecker from "./components/GrammarChecker";
+import grammarImage from "./assets/grammar_2.png";
 
 function App() {
   return (
     <>
       <GrammarChecker />
+      <img id="bottom-image" src={grammarImage} alt="Bottom Image" />
     </>
   );
 }
