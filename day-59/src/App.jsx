8ba5d2296c7c44ae5e17f4014ad5f8c0969f5ee8
Aman kureshi <amankureshi78692@gmail.com>
2025-09-19 11:14:18 +0530

    added heading and grammar icon

--- day-59/src/App.jsx
+++ day-59/src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
 import GrammarChecker from "./components/GrammarChecker";
 import grammarImage from "./assets/grammar_2.png";
+import { TbTextGrammar } from "react-icons/tb";
 
 function App() {
   return (
-    <>
-      <GrammarChecker />
-      <img id="bottom-image" src={grammarImage} alt="Bottom Image" />
-    </>
+    <div className="relative min-h-screen flex flex-col items-center justify-center px-4">
+      <h1 className="flex items-center justify-center mb-6 font-bold text-gray-800 sm:text-1xl md:text-3xl lg:text-4xl">
+        <TbTextGrammar className="mr-2" />
+        Grammar
+        <span className="text-pink-700 ml-2">Checker</span>
+      </h1>
+      <div className="w-full max-w-2xl bg-white/70 backdrop-blur-lg shadow-xl rounded-2xl p-6 relative z-10">
+        <GrammarChecker />
+      </div>
+      <img
+        id="bottom-image"
+        src={grammarImage}
+        alt="Grammar Illustration"
+        className="pointer-events-none select-none"
+      />
+    </div>
   );
 }
 
