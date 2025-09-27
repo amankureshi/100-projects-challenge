118042c6917e7cc63b509f2962e635534c2a7aea
Aman kureshi <amankureshi78692@gmail.com>
2025-09-27 21:40:18 +0530

    added basic html tag in Game.tsx component

---
+++ day-60/src/components/Game.tsx
@@ -0,0 +1,15 @@
+
+
+const Game = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-6">Word Scramble Game</h1>
+            <p className="text-lg">time here</p>
+            <p className="text-lg">score:</p>
+            <h2>scrambled</h2>
+            <input type="text" className="mt-4 p-2 text-black rounded-md" />
+        </div>
+    )
+}
+
+export default Game
