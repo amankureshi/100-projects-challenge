497fd1aedd13878454129a35258464eb34491d7a
Aman kureshi <amankureshi78692@gmail.com>
2025-10-03 19:03:07 +0530

    Add timer component with auto-decrement every second

--- day-60/src/components/Timer.jsx
+++ day-60/src/components/Timer.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Timer = () => {
+const Timer: React.FC<TimerProps> = ({ time, setTime }) => {
+  useEffect(() => {
+    if (time > 0) {
+      const interval = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [time, setTime]);
   return <div>Time left:</div>;
 };
 
