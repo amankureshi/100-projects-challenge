6d540534913090b500ffc35ef3053e14a54c47a9
Aman kureshi <amankureshi78692@gmail.com>
2025-10-07 18:45:48 +0530

    Fix: Prevent unnecessary intervals when time is 0 or less

--- day-60/src/components/Timer.tsx
+++ day-60/src/components/Timer.tsx
@@ -7,15 +7,12 @@ interface TimerProps {
 
 const Timer: React.FC<TimerProps> = ({ time, setTime }) => {
   useEffect(() => {
-    if (time > 0) {
-      const interval = setInterval(() => {
-        setTime((prev) => prev - 1);
-      }, 1000);
-      return () => clearInterval(interval);
-    }
-  }, [time, setTime]);
+    if (time <= 0) return;
+    const interval = setInterval(() => setTime((t) => t - 1), 1000);
+    return () => clearInterval(interval);
+  }, [time]);
   return (
-    <div className="text-lg font-semibold">
+    <div className="text-lg font-semibold bg-white text-indigo-700 px-4 py-1 rounded-full shadow">
       Time left: <span className="text-yellow-300">{time}s</span>
     </div>
   );
