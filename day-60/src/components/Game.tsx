f8876be069200bbd7c5df776478dd65702d08a99
Aman kureshi <amankureshi78692@gmail.com>
2025-10-10 21:40:38 +0530

    Remove h2 and new section added for P and Timer

--- day-60/src/components/Game.tsx
+++ day-60/src/components/Game.tsx
@@ -61,12 +61,12 @@ const Game: React.FC = () => {
         <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-indigo-700 to-purple-800 text-white px-4">
             <div className="flex justify-between items-center w-full max-w-md mb-6">
                 <h1 className="text-3xl font-bold mb-6">Word Scramble Game</h1>
-                <Timer time={time} setTime={setTime} />
             </div>
             <div className="bg-white text-gray-800 rounded-2xl shadow-lg p-6 w-full max-w-md text-center">
-                <h2 className="text-2xl mt-4 tracking-widest font-mono">Word scarmable Game</h2>
-                <p className="text-3xl font-bold text-indigo-700 tracking-widest mb-6">{scrambled}</p>
-
+                <section className="flex justify-between">
+                    <p className="text-3xl font-bold text-indigo-700 tracking-widest mb-6">{scrambled}</p>
+                    <Timer time={time} setTime={setTime} />
+                </section>
                 <input type="text" className="w-full border rounded-lg px-4 py-2 text-center text-lg focus:outline-none focus:ring-2 focus:ring-indigo-500 mb-4" onChange={(e) => setInput(e.target.value)} value={input} />
                 <div className="flex justify-center gap-4 mt-4">
                     <button
