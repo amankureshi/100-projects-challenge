0cdcc82ffbdeee2f23d4ffbbe089fb9c7338e48e
Aman kureshi <amankureshi78692@gmail.com>
2025-10-11 21:05:16 +0530

    remove a bg-white class

--- day-60/src/components/Timer.tsx
+++ day-60/src/components/Timer.tsx
@@ -11,8 +11,9 @@ const Timer: React.FC<TimerProps> = ({ time, setTime }) => {
     const interval = setInterval(() => setTime((t) => t - 1), 1000);
     return () => clearInterval(interval);
   }, [time]);
+
   return (
-    <div className="text-lg font-semibold bg-white text-indigo-700 px-4 py-1 rounded-full shadow">
+    <div className="text-lg font-semibold text-indigo-700">
       Time left: <span className="text-yellow-300">{time}s</span>
     </div>
   );
