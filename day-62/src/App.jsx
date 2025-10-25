dda309beb4bce361c6cac287e01b574f15445e96
Aman kureshi <amankureshi78692@gmail.com>
2025-10-25 20:41:25 +0530

    added day-62

---
+++ day-62/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
+        📝 Sentence Rephraser
+      </h1>
+    </div>
+  );
+}
+
+export default App;
