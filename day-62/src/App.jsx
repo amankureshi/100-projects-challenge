cb5e8849882f83b1fa79b4588b29e786834277d4
Aman kureshi <amankureshi78692@gmail.com>
2025-10-28 09:15:01 +0530

    added useState and async function

--- day-62/src/App.jsx
+++ day-62/src/App.jsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState();
+  const [rephrased, setRephrased] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleReph()() {
+    if(!text.trim()) return alert("Please enter a sentence")
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
-        📝 Sentence Rephraser
-      </h1>
-      <textarea placeholder="Type a sentence to rephrase..." rows={5} />
-      <div className="flex justify-between mt-4">
-        <button className="px-4 py-2 rounded-md border">Clear</button>
-      </div>
-      <div className="mt-6 bg-indigo-50 p-4 rounded-lg border border-indigo-100">
-        <h3 className="font-semibold text-indigo-700 mb-2">
-          ✨ Rephrased Sentence:
-        </h3>
-        <p className="text-gray-800 text-sm">rephrased here</p>
-        <button className="mt-3 text-xs px-3 py-1 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition">
-          Copy
-        </button>
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="bg-white shadow-2xl rounded-2xl p-6 w-full max-w-lg">
+        <h1 className="text-3xl font-bold text-center text-indigo-700 mb-6">
+          📝 Sentence Rephraser
+        </h1>
+        <textarea placeholder="Type a sentence to rephrase..." rows={5} />
+        <div className="flex justify-between mt-4">
+          <button className="px-4 py-2 rounded-md border">Clear</button>
+        </div>
+        <div className="mt-6 bg-indigo-50 p-4 rounded-lg border border-indigo-100">
+          <h3 className="font-semibold text-indigo-700 mb-2">
+            ✨ Rephrased Sentence:
+          </h3>
+          <p className="text-gray-800 text-sm">rephrased here</p>
+          <button className="mt-3 text-xs px-3 py-1 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition">
+            Copy
+          </button>
+        </div>
       </div>
     </div>
   );
