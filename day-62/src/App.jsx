f6bd4cbdf8f37d41acaa3f7e7e2521f467f1a507
Aman kureshi <amankureshi78692@gmail.com>
2025-11-04 15:48:03 +0530

    created handleClear function and onCLick

--- day-62/src/App.jsx
+++ day-62/src/App.jsx
@@ -30,6 +30,12 @@ function App() {
       setLoading(false);
     }
   }
+
+  function handleClear() {
+    setText("");
+    setRephrased("");
+  }
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center p-4">
       <div className="bg-white shadow-2xl rounded-2xl p-6 w-full max-w-lg">
@@ -38,7 +44,9 @@ function App() {
         </h1>
         <textarea placeholder="Type a sentence to rephrase..." rows={5} />
         <div className="flex justify-between mt-4">
-          <button className="px-4 py-2 rounded-md border">Clear</button>
+          <button onClick={handleClear} className="px-4 py-2 rounded-md border">
+            Clear
+          </button>
         </div>
         <div className="mt-6 bg-indigo-50 p-4 rounded-lg border border-indigo-100">
           <h3 className="font-semibold text-indigo-700 mb-2">
