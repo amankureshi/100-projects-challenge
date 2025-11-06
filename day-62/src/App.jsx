d0a45b80b3ae3dfef3818fc4222034552fd62a1f
Aman kureshi <amankureshi78692@gmail.com>
2025-11-06 22:20:11 +0530

    added onClick event in copy button

--- day-62/src/App.jsx
+++ day-62/src/App.jsx
@@ -58,7 +58,10 @@ function App() {
             ✨ Rephrased Sentence:
           </h3>
           <p className="text-gray-800 text-sm">rephrased here</p>
-          <button className="mt-3 text-xs px-3 py-1 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition">
+          <button
+            onClick={handleCopy}
+            className="mt-3 text-xs px-3 py-1 bg-indigo-600 text-white rounded-full hover:bg-indigo-700 transition"
+          >
             Copy
           </button>
         </div>
