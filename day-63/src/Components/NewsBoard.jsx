78e7dc1eb17cc9dcd393ef44bda0262ff85fd243
Aman kureshi <amankureshi78692@gmail.com>
2026-02-16 22:30:16 +0530

    Implement news fetching using useEffect and useState

--- day-63/src/Components/NewsBoard.jsx
+++ day-63/src/Components/NewsBoard.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+
 const NewsBoard = () => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    fetch(url)
+      .then((response) => response.json.data())
+      .then((data) => setArticles(data.articles));
+  }, []);
   return (
-    <>
-      <h2>NewdBoard</h2>
-    </>
+    <di>
+      <h2 className="text-center">
+        Latest<span className="badge bg-danger">News</span>
+      </h2>
+    </di>
   );
 };
 
