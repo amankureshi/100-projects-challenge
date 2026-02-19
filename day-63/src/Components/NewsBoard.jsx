6d8c277813200766df733b4c56595cbe54459b82
Aman kureshi <amankureshi78692@gmail.com>
2026-02-19 13:11:08 +0530

    feat: add category prop for dynamic news filtering

--- day-63/src/Components/NewsBoard.jsx
+++ day-63/src/Components/NewsBoard.jsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
-const NewsBoard = () => {
+const NewsBoard = ({ category }) => {
   console.log(import.meta.env);
   console.log(import.meta.env.VITE_API_KEY);
 
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => setArticles(data.articles));
