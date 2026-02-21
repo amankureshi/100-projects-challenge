49271a48ec4f583e5f02e8ae3019ca233bd7f226
Aman kureshi <amankureshi78692@gmail.com>
2026-02-21 13:46:38 +0530

    pass a props setCategory in Navbar and added Social icons

--- day-63/src/Components/Navabar.jsx
+++ day-63/src/Components/Navabar.jsx
@@ -1,45 +1,92 @@
-const Navabar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <span className="badge bg-light text-dark fs-4">Navbar</span>
-        </a>
+import { IoLogoInstagram } from "react-icons/io5";
+import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
+const Navabar = ({ setCategory }) => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-lg container-fluid p-4">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <div className="d-flex gap-4">
+            <a href="#" className="text-dark fs-5">
+              <IoLogoInstagram />
             </a>
-            <a className="nav-link" href="#">
-              Features
+            <a href="#" className="text-dark fs-5">
+              <FaLinkedinIn />
             </a>
-            <a className="nav-link" href="#">
-              Pricing
+            <a href="#" className="text-dark fs-5">
+              <FaFacebookF />
             </a>
-            <a
-              className="nav-link disabled"
-              href="#"
-              tabIndex="-1"
-              aria-disabled="true"
-            >
-              Disabled
+            <a href="#" className="text-dark fs-5">
+              <FaGithub />
             </a>
           </div>
+
+          <h1 className="navbar-brand mx-auto text-uppercase fs-2" href="#">
+            Sirat<span className="text-danger"> News</span>
+          </h1>
+
+          <button className="btn btn-dark rounded-0 px-4 py-3.5 fs-6 subscribeBtn">
+            Subscribe
+          </button>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      <nav className="navbar navbar-expand-lg border-top border-bottom border-dark bg-light">
+        <div className="container-fluid p-3">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div
+            className="collapse navbar-collapse pointer fw-bold"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav mx-auto text-center gap-3">
+              <div
+                className="nav-link text-black"
+                onClick={() => setCategory("technology")}
+              >
+                Technology
+              </div>
+              <div
+                className="nav-link text-black"
+                onClick={() => setCategory("business")}
+              >
+                Business
+              </div>
+              <div
+                className="nav-link text-black"
+                onClick={() => setCategory("health")}
+              >
+                Health
+              </div>
+              <div
+                className="nav-link text-black"
+                onClick={() => setCategory("sports")}
+              >
+                Sports
+              </div>
+              <div
+                className="nav-link text-black"
+                onClick={() => setCategory("entertainment")}
+              >
+                Entertainment
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 export default Navabar;
