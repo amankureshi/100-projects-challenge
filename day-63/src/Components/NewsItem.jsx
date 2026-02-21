7e6502f296841948c3802b9b5a034b8b3e846df5
Aman kureshi <amankureshi78692@gmail.com>
2026-02-21 19:22:48 +0530

    feat: add skeleton loading UI and image fallback support

--- day-63/src/Components/NewsItem.jsx
+++ day-63/src/Components/NewsItem.jsx
@@ -1,22 +1,37 @@
+import { useState } from "react";
+import NewsImage from "../assets/news.jpg";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 const NewsItem = ({ title, description, src, url }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
     <div className="card h-100 border-0 shadow-sm news-card">
+      {!imageLoaded && <Skeleton height={220} />}
       <img
-        src={src || "https://via.placeholder.com/400x250"}
+        src={src || NewsImage}
         className="card-img-top"
         alt="news"
-        style={{ height: "220px", objectFit: "cover" }}
+        style={{
+          height: "220px",
+          objectFit: "cover",
+          display: imageLoaded ? "block" : "none",
+        }}
+        onLoad={() => setImageLoaded(true)}
+        onError={() => setImageLoaded(true)}
       />
 
       <div className="card-body d-flex flex-column">
         <h5 className="card-title fw-semibold mb-3">
-          {title?.slice(0, 60)}...
+          {title ? title?.slice(0, 60) + "..." : <Skeleton width={150} />}
         </h5>
 
         <p className="card-text text-muted flex-grow-1">
-          {description
-            ? description.slice(0, 100) + "..."
-            : "No description available."}
+          {description ? (
+            description.slice(0, 100) + "..."
+          ) : (
+            <Skeleton count={3} />
+          )}
         </p>
 
         <a
