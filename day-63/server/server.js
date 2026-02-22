9e236db2ebf6732cf1206aa3b610a7fbd7ef9f79
Aman kureshi <amankureshi78692@gmail.com>
2026-02-22 22:15:57 +0530

    feat(backend): initialize backend server using Node.js and Express with REST API and middleware

---
+++ day-63/server/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.get("/news", async (req, res) => {
+  try {
+    const category = req.query.category || "technology";
+
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.API_KEY}`,
+    );
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
